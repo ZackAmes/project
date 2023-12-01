a81c4eab4e2684923d5635e3803f0c4b2cf04909
Zack Ames <zames6584@gmail.com>
2023-12-01 09:42:37 -0500

    swap to and improve board component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,9 @@ import { Entity } from "@dojoengine/recs";
 import { setComponentsFromGraphQLEntities, getEntityIdFromKeys } from "@dojoengine/utils";
 import Secret from "./components/Secret";
 import Button from "./components/Button";
-import Square from "./components/Square";
+import Board from "./components/Board";
 import {Canvas} from '@react-three/fiber';
-import { MapControls } from "@react-three/drei";
+import { FlyControls } from "@react-three/drei";
 
 function App() {
   const {
@@ -32,7 +32,7 @@ function App() {
   // get current component values
   const secret = useComponentValue(components.Secret, entityId as Entity);
   
-  const squares = [];
+  const squareStates = [];
   const squareIds = [];
 
   for(let i=0; i<3; i++){
@@ -42,11 +42,11 @@ function App() {
       tempIds.push(getEntityIdFromKeys([BigInt(0),BigInt(i),BigInt(j)]))
       tempSquares.push(useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(i),BigInt(j)])))
     }
-    squares.push(tempSquares)
+    squareStates.push(tempSquares)
     squareIds.push(tempIds)
   }
 
-  console.log(squares)
+  console.log(squareStates)
   console.log(squareIds)
   const square00 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(0),BigInt(0)]));
   const square01 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(0),BigInt(1)]));  
@@ -59,7 +59,7 @@ function App() {
   const square22 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(2),BigInt(2)]));  
  
   // use graphql to current state data
-  useSync(torii_client, SecretContract, [entityId])
+  //useSync(torii_client, SecretContract, [entityId])
   // useSync(torii_client, SquareContract, squareIds.flat())
 
   return (
@@ -79,46 +79,13 @@ function App() {
       </div>
       <div id="canvas-container">
         <Canvas style={{height: 800, width:800}} camera={{ position: [0, 0, 5], zoom: 1, up: [0, 0, 1], far: 10000 }}>
-          <MapControls/>
+          <FlyControls dragToLook={true} />
           <pointLight position={[10, 10, 10]} />
           <Button x={2.2} y={0} z={0} label={"spawn"} click={() => spawn(account)}/>
           <Button x={-2.2} y={2} z={0} label={"clear"} click={clear}/>
           <Button x={2.2} y={2} z={0} label={"create"} click={create}/>
-          <Square x={0} y={0} z={0} 
-            state={squares[1][1] ? squares[1][1].state : " "}
-            color="green" click={() => takeTurn(account, "0", 1, 1)}
-          />
-          <Square x={1} y={0} z={0} 
-            state={square21 ? `${square21["state"]}` : " "}
-            color="blue" click={() => takeTurn(account, "0", 2, 1)}
-          />
-          <Square x={-1} y={0} z={0} 
-            state={square01 ? `${square01["state"]}` : " "} 
-            color="black" click={() => takeTurn(account, "0", 0, 1)}
-          />
-          <Square x={0} y={1} z={0} 
-            state={square12 ? `${square12["state"]}` : " "} 
-            color="black" click={() => takeTurn(account, "0", 1, 2)}
-          />
-          <Square x={1} y={1} z={0} 
-            state={square22 ? `${square22["state"]}` : " "} 
-            color="black" click={() => takeTurn(account, "0", 2, 2)}
-          />
-          <Square x={-1} y={1} z={0} 
-            state={square02 ? `${square02["state"]}` : " "} 
-            color="black" click={() => takeTurn(account, "0", 0, 2)}
-          />
-          <Square x={0} y={-1} z={0} 
-            state={square10 ? `${square10["state"]}` : " "} 
-            color="black" click={() => takeTurn(account, "0", 1, 0)}
-          />
-          <Square x={1} y={-1} z={0} 
-            state={square20 ? `${square20["state"]}` : " "} 
-            color="black" click={() => takeTurn(account, "0", 2, 0)}
-          />
-          <Square x={-1} y={-1} z={0}
-            state={square00 ? `${square00["state"]}` : " "}
-            color="black" click={() => takeTurn(account, "0", 0, 0)}
+          <Board account={account} game_id="0" components={components} 
+                 takeTurn={takeTurn} coords={[0,0,0]} squareStates={squareStates}
           />
           <Secret value={secret ? parseInt(`${secret["value"]}`) : 0} click={() => setSecret(account, 42)}/>
 
--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,6 +1,4 @@
 import {FC} from 'react';
-import { useComponentValue } from '@latticexyz/react';
-import { getEntityIdFromKeys } from '@dojoengine/utils';
 import Square from './Square';
 
 
@@ -9,80 +7,56 @@ interface BoardProps {
     components: any
     takeTurn: any
     account: any
+    coords: number[]
+    squareStates: any[]
 }
 
 
 
-const Board: FC<BoardProps> = ({game_id, components, account, takeTurn}) => {
-  
-
-    const square00 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(0),BigInt(0)]));
-    const square01 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(0),BigInt(1)]));  
-    const square02 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(0),BigInt(2)]));  
-    const square10 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(1),BigInt(0)]));  
-    const square11 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(1),BigInt(1)]));  
-    const square12 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(1),BigInt(2)]));  
-    const square20 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(2),BigInt(0)]));  
-    const square21 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(2),BigInt(1)]));  
-    const square22 = useComponentValue(components.Square, getEntityIdFromKeys([BigInt(0),BigInt(2),BigInt(2)]));  
+const Board: FC<BoardProps> = ({game_id, components, account, takeTurn, coords, squareStates}) => {
+    
+    //coords for center of board
+    let x = coords[0]
+    let y = coords[1]
+    let z = coords[2]
 
     return (
         <>
-        <mesh position={[.5, 0, -1]}>
-            <boxGeometry args={[.01, 3, .1]} />
-            <meshBasicMaterial color="red"/>
-            <boxGeometry args={[3, .01, .1]} />
-        </mesh>
-        <mesh position={[-.5, 0, -1]}>
-            <boxGeometry args={[.01, 3, .1]} />
-            <meshBasicMaterial color="red"/>
-        </mesh>
-        <mesh position={[0, .5, -1]}>
-            <meshBasicMaterial color="red"/>
-        </mesh>
-        <mesh position={[0, -.5, -1]}>
-            <boxGeometry args={[3, .01, .1]} />
-            <meshBasicMaterial color="red"/>
-        </mesh>
-        <mesh position ={[0,0,-3.5]}>
-            <boxGeometry args={[5, 5, .1]}/>
-            <meshBasicMaterial color = "blue"/>
-        </mesh>
         <Square x={0} y={0} z={0} 
-            state={square11 ? `${square11["state"]}` : " "}
-            color="white" click={() => takeTurn(account, "0", 1, 1)}
+            state={squareStates[1][1]? squareStates[1][1].state : " "}
+            color="red" click={() => takeTurn(account, "0", 1, 1)}
         />
         <Square x={1} y={0} z={0} 
-            state={square21 ? `${square21["state"]}` : " "}
-            color="white" click={() => takeTurn(account, "0", 2, 1)}
+            state={squareStates[2][1]? squareStates[2][1].state : " "}
+            color="blue" click={() => takeTurn(account, "0", 2, 1)}
         />
         <Square x={-1} y={0} z={0} 
-            state={square01 ? `${square01["state"]}` : " "} 
-            color="white" click={() => takeTurn(account, "0", 0, 1)}
+            state={squareStates[1][1]? squareStates[0][1].state : " "} 
+            color="blue" click={() => takeTurn(account, "0", 0, 1)}
         />
         <Square x={0} y={1} z={0} 
-            state={square12 ? `${square12["state"]}` : " "} 
-            color="white" click={() => takeTurn(account, "0", 1, 2)}
+            state={squareStates[1][1]? squareStates[1][2].state : " "} 
+            color="blue" click={() => takeTurn(account, "0", 1, 2)}
         />
         <Square x={1} y={1} z={0} 
-            state={square22 ? `${square22["state"]}` : " "} 
-            color="white" click={() => takeTurn(account, "0", 2, 2)}
+            state={squareStates[1][1]? squareStates[2][2].state : " "} 
+            color="red" click={() => takeTurn(account, "0", 2, 2)}
         />
         <Square x={-1} y={1} z={0} 
-            state={square02 ? `${square02["state"]}` : " "} 
-            color="white" click={() => takeTurn(account, "0", 0, 2)}
+            state={squareStates[1][1]? squareStates[0][2].state : " "} 
+            color="red" click={() => takeTurn(account, "0", 0, 2)}
         />
         <Square x={0} y={-1} z={0} 
-            state={square10 ? `${square10["state"]}` : " "} 
-            color="white" click={() => takeTurn(account, "0", 1, 0)}
+            state={squareStates[1][1]? squareStates[1][0].state : " "} 
+            color="blue" click={() => takeTurn(account, "0", 1, 0)}
         />
         <Square x={1} y={-1} z={0} 
-            state={square20 ? `${square20["state"]}` : " "} 
-            color="white" click={() => takeTurn(account, "0", 2, 0)}
+            state={squareStates[1][1]? squareStates[2][0].state : " "} 
+            color="red" click={() => takeTurn(account, "0", 2, 0)}
         />
         <Square x={-1} y={-1} z={0}
-            state={square00 ? `${square00["state"]}` : " "}
-            color="white" click={() => takeTurn(account, "0", 0, 0)}
+            state={squareStates[1][1]? squareStates[0][0].state : " "}
+            color="red" click={() => takeTurn(account, "0", 0, 0)}
         />
         </>
     )
